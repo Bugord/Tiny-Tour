fbd870de30f2035205c6cb2cab4ae841364fae0e
Bugord <yury.kakareka@gmail.com>
2024-10-01 02:21:57 +0200

    Added new UI toggles

---
+++ Assets/Scripts/Common/UI/SwitchableToggle.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common.UI
+{
+    public class SwitchableToggle : MonoBehaviour
+    {
+        [SerializeField]
+        private Toggle toggle;
+        
+        [Header("Off State")]
+        [SerializeField]
+        private Sprite offSprite;
+        
+        [SerializeField]
+        private SpriteState offSpriteState;
+        
+        private Sprite onSprite;
+        private SpriteState onSpriteState;
+
+        private void Awake()
+        {
+            onSprite = toggle.image.sprite;
+            onSpriteState = toggle.spriteState;
+            toggle.onValueChanged.AddListener(OnToggleChanged);
+
+            if (!toggle.isOn) {
+                toggle.image.sprite = offSprite;
+            }
+        }
+
+        private void OnToggleChanged(bool isOn)
+        {
+            toggle.spriteState = isOn ? onSpriteState : offSpriteState;
+            toggle.image.sprite = isOn ? onSprite : offSprite;
+        }
+    }
+}
--- Assets/Scripts/Gameplay/UI/PlayControllerUI.cs
+++ Assets/Scripts/Gameplay/UI/PlayControllerUI.cs
@@ -1,16 +1,9 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 namespace Gameplay.UI
 {
     public class PlayControllerUI : MonoBehaviour
     {
-        [SerializeField]
-        private Toggle playToggle;
-
-        public void OnPlayTogglePressed()
-        {
-            playToggle.targetGraphic.enabled = !playToggle.isOn;
-        }
+        
     }
 }
