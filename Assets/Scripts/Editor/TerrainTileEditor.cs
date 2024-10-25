aa35895f3d4aa58c261af310f1115a5763a9c737
Bugord <kakareko.yurij@yandex.ru>
2024-10-25 02:29:41 +0200

    Minor level and terrain tile fixes

--- Assets/Scripts/Editor/TerrainTileEditor.cs
+++ Assets/Scripts/Editor/TerrainTileEditor.cs
@@ -1,4 +1,5 @@
-﻿using Tiles.Ground;
+﻿using System;
+using Tiles.Ground;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,18 +14,29 @@ namespace Editor
     {
         private const string WaterIconBase64 =
             "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAsSURBVDhPY6AEMIKI0sf//4N5JIBuWUZGJiibLDCqmUQwqplEQJFmCgADAwBOCAQazbgI9AAAAABJRU5ErkJggg==";
-     
+
         private const string GroundIconBase64 =
             "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAsSURBVDhPY6AEMIIIl6MZ/8E8EsAe6xmMTFA2WWBUM4lgVDOJgCLNFAAGBgAfUAQar5o52gAAAABJRU5ErkJggg==";
-     
+
         private const string WaterOrBridgeBaseIconBase64 =
             "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAA3SURBVDhPY6AEMIKI0sf//4N5aOD18nUMP+89hPJQwfIZRYxMUDZZYFQziWBUM4mAIs0UAAYGAHdlChrh2lTjAAAAAElFTkSuQmCC";
-  
+
         private const string GroundOrBridgeBaseIconBase64 =
             "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAA3SURBVDhPY6AEMIKI0sf//4N5aOD18nUMP+89hPJQwfIZRYxMUDZZYFQziWBUM4mAIs0UAAYGAHdlChrh2lTjAAAAAElFTkSuQmCC";
-  
+
         private const string BridgeBaseIconBase64 =
             "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAsSURBVDhPY6AEMIKIyIy+/2AeCWD5jCJGJiibLDCqmUQwqplEQJFmCgADAwAYaAQawG1sEgAAAABJRU5ErkJggg==";
+
+        /// <summary>
+        /// Draws a Sprite field for the Rule
+        /// </summary>
+        /// <param name="rect">Rect to draw Sprite Inspector in</param>
+        /// <param name="tilingRule">Rule to draw Sprite Inspector for</param>
+        public override void SpriteOnGUI(Rect rect, RuleTile.TilingRuleOutput tilingRule)
+        {
+            var newTileRect = new Rect(rect.x, rect.y, 55, 55);
+            tilingRule.m_Sprites[0] = EditorGUI.ObjectField(newTileRect, GUIContent.none, tilingRule.m_Sprites[0], typeof(Sprite), false) as Sprite;
+        }
 
         /// <summary>
         /// Draws a neighbor matching rule
@@ -43,7 +55,7 @@ namespace Editor
                     break;
                 case TerrainTile.Neighbor.Ground:
                     GUI.DrawTexture(rect, Base64ToTexture(GroundIconBase64));
-                    break;        
+                    break;
                 case TerrainTile.Neighbor.WaterOrBridgeBase:
                     GUI.DrawTexture(rect, Base64ToTexture(WaterOrBridgeBaseIconBase64));
                     break;
@@ -52,7 +64,7 @@ namespace Editor
                     break;
                 case TerrainTile.Neighbor.Water:
                     GUI.DrawTexture(rect, Base64ToTexture(WaterIconBase64));
-                    break;     
+                    break;
                 case TerrainTile.Neighbor.BridgeBase:
                     GUI.DrawTexture(rect, Base64ToTexture(BridgeBaseIconBase64));
                     break;
